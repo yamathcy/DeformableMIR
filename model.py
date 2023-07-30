6362a7e720b067ba3dab4ad4399e181ab75c7809
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-07-30 11:40:33 +0900

    rename

--- model.py
+++ model.py
@@ -19,15 +19,15 @@ def conv(deform,**kwrds):
     
 
 class CNN(nn.Module):
-    def __init__(self, channels, kernel_size, pooling=[2,2,2,2],sconv=True,num_classes=10,deform=False):
+    def __init__(self, channels, kernel_size, pooling=[2,2,2,2],sconv=True,num_classes=10,deform=False,mod=True):
         super().__init__()
         # print(channels)
         self.inp = MultiResolutionMelSpec(n_mels=128)
         self.conv1 = ConvBlock(in_channels=3, out_channels=channels[0],kernel_size=kernel_size[0],pooling=pooling[0],sconv=sconv)
         self.conv2 = ConvBlock(in_channels=channels[0], out_channels=channels[1],kernel_size=kernel_size[1],pooling=pooling[1],sconv=sconv)
         self.deform = True if deform else False
-        self.conv3 = conv(in_channels=channels[1], out_channels=channels[2],kernel_size=kernel_size[2],pooling=pooling[2],sconv=sconv,deform=deform)
-        self.conv4 = conv(in_channels=channels[2], out_channels=channels[3],kernel_size=kernel_size[3],pooling=pooling[3],sconv=sconv,deform=deform)
+        self.conv3 = conv(in_channels=channels[1], out_channels=channels[2],kernel_size=kernel_size[2],pooling=pooling[2],sconv=sconv,deform=deform,mod=mod)
+        self.conv4 = conv(in_channels=channels[2], out_channels=channels[3],kernel_size=kernel_size[3],pooling=pooling[3],sconv=sconv,deform=deform,mod=mod)
         self.gap = nn.AdaptiveAvgPool2d((1,1))
         self.linear = nn.Linear(channels[3], 50)
         self.norm = nn.InstanceNorm2d(50)
