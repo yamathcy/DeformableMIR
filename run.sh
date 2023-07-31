889c8f0ec176b14f2bf148146a3c63dde4d21aa5
yamathcy <s1921652@s.tsukuba.ac.jp>
2023-07-31 22:03:58 +0900

    rename

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ python main.py sconv=False deform=True retrain=True mel=False experiment_name='d
 python main.py sconv=False deform=True retrain=True mel=False mod=False experiment_name='dcnv1 crt stft'
 python main.py sconv=False deform=True retrain=False mel=False experiment_name='dcnv2 stft'
 python main.py sconv=False deform=True retrain=False mel=False mod=False experiment_name='dcnv1 stft'
-python main.py sconv=False deform=False retrain=False mel=False kernel_size='[(4,1),(16,1),(1,6),(1,16)]' pooling='[2,2,2,2]' experiment_name='OblongCNN'
+python main.py sconv=False deform=False retrain=False mel=False kernel_size="[(4,1),(16,1),(1,6),(1,16)]" pooling="[2,2,2,2]" experiment_name='OblongCNN'
 python main.py sconv=False deform=True retrain=True mel=False kernel_size='[(4,1),(16,1),(1,6),(1,16)]' pooling='[2,2,2,2]' experiment_name='OblongDeformableCNN'
